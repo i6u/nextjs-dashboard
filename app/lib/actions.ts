b1a851845ca4424701463102873aa6fbaca28c09
witt <mymeek@163.com>
2024-07-14 21:15:15 +0800

    fix delete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -109,13 +109,13 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
 
-    // try {
-    //     await sql`DELETE FROM invoices WHERE id = ${id}`;
-    //     revalidatePath('/dashboard/invoices');
-    //     return { message: 'Deleted Invoice.' };
-    // } catch (error) {
-    //     return { message: 'Database Error: Failed to Delete Invoice.' };
-    // }
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
 }
